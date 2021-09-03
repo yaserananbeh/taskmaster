181abed71f8bb6f57db75b838f266d837773167b
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-09-03 18:03:56 +0300

    lab 29 work

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +24,16 @@ public class MainActivity extends AppCompatActivity {
 //        TextView textView = (TextView) findViewById(R.id.textView5);
 //        textView.setText(task.toString());
         // Create some data
-        ArrayList<Task> allTasks = new ArrayList<Task>();
-        allTasks.add(new Task("task 1","do the lab","new"));
-        allTasks.add(new Task("task 2","finish the reading","assigned"));
-        allTasks.add(new Task("task 3","hanging out ","in progress"));
-        allTasks.add(new Task("task 4","Play video games","in progress"));
-        allTasks.add(new Task("task 5","do some resubmissions","complete"));
+//        ArrayList<Task> allTasks = new ArrayList<Task>();
+//        allTasks.add(new Task("task 1","do the lab","new"));
+//        allTasks.add(new Task("task 2","finish the reading","assigned"));
+//        allTasks.add(new Task("task 3","hanging out ","in progress"));
+//        allTasks.add(new Task("task 4","Play video games","in progress"));
+//        allTasks.add(new Task("task 5","do some resubmissions","complete"));
+        AppDatabase appDatabase;
+        appDatabase= Room.databaseBuilder(getApplicationContext(),AppDatabase.class,"tasks").allowMainThreadQueries().build();
+        TaskDao taskDao=appDatabase.taskDao();
+        List<Task> allTasks=taskDao.getAll();
         RecyclerView allTasksRecycleView = findViewById(R.id.TaskListRecyclerView);
         allTasksRecycleView.setLayoutManager(new LinearLayoutManager(this));
         allTasksRecycleView.setAdapter(new TaskAdapter(allTasks));
@@ -39,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(renderAllTasksActivity);
 //            }
 //        });
-//        Button addTaskButton = findViewById(R.id.addTask);
-//        addTaskButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View V) {
-//                Intent renderAllTasksActivity = new Intent(MainActivity.this, AddTask.class);
-//                startActivity(renderAllTasksActivity);
-//            }
-//        });
+        Button addTaskButton = findViewById(R.id.addTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent renderAllTasksActivity = new Intent(MainActivity.this, AddTask.class);
+                startActivity(renderAllTasksActivity);
+            }
+        });
 //        Button task2Btn = findViewById(R.id.Task2Btn);
 //        task2Btn.setOnClickListener((view -> {
 //            String taskTitle = task2Btn.getText().toString();
--- app/src/main/java/com/example/taskmaster/Task.java
+++ app/src/main/java/com/example/taskmaster/Task.java
@@ -1,26 +1,36 @@
 package com.example.taskmaster;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.util.ArrayList;
-
+@Entity
 public class Task {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+    @ColumnInfo(name = "title")
     public String title;
+    @ColumnInfo(name = "body")
     public String body;
+    @ColumnInfo(name = "chosenState")
     public String chosenState;
-    public ArrayList<String> state =new ArrayList<String>(){
-        {
-            add("new");
-            add("assigned");
-            add("in progress");
-            add("complete");
-        }
-
-    };
+//    public ArrayList<String> state =new ArrayList<String>(){
+//        {
+//            add("new");
+//            add("assigned");
+//            add("in progress");
+//            add("complete");
+//        }
+//
+//    };
 
 
     public Task(String title, String body, String chosenState) {
         this.title = title;
         this.body = body;
-        this.chosenState = this.state.get(state.indexOf(chosenState.toLowerCase()));
+//        this.chosenState = this.state.get(state.indexOf(chosenState.toLowerCase()));
+        this.chosenState = chosenState;
     }
 
     @Override
